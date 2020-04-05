12f62410930b50b163b217f1625b92b543a2dc9b
abhishekmohan2012 <abhishek.mohan2012@gmail.com>
2020-04-05 13:03:59 +0530

    First Commit

---
+++ src/main/java/com/security/authenticationApplicaitonProperties/AuthenticationApplicaitonPropertiesApplication.java
@@ -0,0 +1,13 @@
+package com.security.authenticationApplicaitonProperties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationApplicaitonPropertiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationApplicaitonPropertiesApplication.class, args);
+	}
+
+}
